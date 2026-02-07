84b782579ccd4d42a8fc203b553212846f8a13e3
Jayne <jayneekani0@com>
2026-02-07 14:18:33 +0000

    Css files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Header from './components/Header';
-import Marketplace from './components/Marketplace';
+import Header from './components/Header/Header';
+import Marketplace from './components/Marketplace/Marketplace';
 
 function App() {
   return (
